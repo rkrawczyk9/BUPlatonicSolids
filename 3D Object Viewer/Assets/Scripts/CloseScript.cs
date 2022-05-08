4aa3cb82de1abaaf092b32c81c3e8b1974da4a1a
BenSchu438 <70485582+BenSchu438@users.noreply.github.com>
2022-05-08 17:24:14 -0500

    builds
    Created builds for the build tool

---
+++ 3D Object Viewer/Assets/Scripts/CloseScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseScript : MonoBehaviour
+{
+    public void CloseProgram()
+    {
+        Application.Quit();
+    }
+}
